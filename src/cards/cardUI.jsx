792aea42b131ef3165a13c9b44307a2fdb71d286
vineeth12800 <59262807+vineeth12800@users.noreply.github.com>
2020-04-20 16:42:48 +0530

    Add files via upload

---
+++ src/cards/cardUI.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./card-style.css";
+
+const cards = (props) => {
+  return (
+    <div className="card text-center shadow">
+      <div className="overflow">
+        <img src={props.imgsrc} alt="Image 1" className="card-img-top" />
+      </div>
+      <div className="card-body text-dark">
+        <h4 className="card-title">{props.title}</h4>
+        <a href="#" className="btn btn-outline-success">
+          Download{" "}
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default cards;
---
+++ src/cards/cards.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Card from "./cardUI";
+import img1 from "../images/download.jpg";
+import img2 from "../images/download1.jpg";
+import img3 from "../images/download2.jpg";
+
+class cards extends Component {
+  render() {
+    return (
+      <div className="container-fluid d-flex justify-content-center">
+        <div className="row">
+          <div className="col-md-4">
+            <Card imgsrc={img1} title="Author 1" />
+          </div>
+          <div className="col-md-4">
+            <Card imgsrc={img2} title="Author 2" />
+          </div>
+          <div className="col-md-4">
+            <Card imgsrc={img3} title="Author 3" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default cards;
